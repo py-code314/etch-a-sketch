9ba8f9ce641a294ebf75f7429dcc25f9ff3203bd
Srikanth K <122169588+py-code314@users.noreply.github.com>
2024-07-15 16:41:36 -0400

    Functions to generate cells in grid pattern
    Generate default grid (16 x 16) on page load.
    Update grid by the number inputted by user.
    Add event listeners for page load & user input.

---
+++ main.js
@@ -0,0 +1,32 @@
+const input = document.querySelector(".form");
+const container = document.querySelector(".grid-container");
+
+document.addEventListener('DOMContentLoaded', defaultGrid)
+function defaultGrid() {
+    const cellNumber = 16
+    generateGrid(cellNumber)
+}
+
+input.addEventListener('submit', updateGrid)
+function updateGrid(event) {
+    event.preventDefault()
+
+    let cellNumber = +(event.target[0].value)
+    container.innerHTML = ""
+    generateGrid(cellNumber)
+}
+
+function generateGrid(cellNumber) {
+    let cellWidth = (container.offsetWidth - 10) / cellNumber;
+    let cellHeight = (container.offsetHeight - 10) / cellNumber;
+    let gridDimensions = cellNumber * cellNumber;
+
+    for (let i = 0; i < gridDimensions; i++) {
+        const cell = document.createElement("div");
+        cell.className = "cell";
+        cell.style.width = `${cellWidth}px`;
+        cell.style.height = `${cellHeight}px`;
+
+        container.appendChild(cell);
+    }
+}
